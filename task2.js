2f11d2de371dcd9c25c22a894282ce0068aa6486
Ankita1925 <ankitasawant22123071@gmail.com>
2026-01-07 20:52:45 +0530

    Implement shop and home functions for display control
    Added functionality to show and hide shop cards and main sections.

--- task2.js
+++ task2.js
@@ -1,6 +1,24 @@
-let card = document.querySelectorAll(".wearing");
+let wearing = document.querySelectorAll(".wearing");
 let mainPage = document.querySelectorAll(".main");
-function shop() {
-    mainPage.style.display = "none";
 
-} 
+function shop() {
+    // hide main home section
+    mainPage.forEach(section => {
+        section.style.display = "none";
+    });
+
+    // show shop cards
+    wearing.forEach(section => {
+        section.style.display = "block";
+    });
+}
+
+function home() {
+    mainPage.forEach(section => {
+        section.style.display = "flex";
+    });
+
+    wearing.forEach(section => {
+        section.style.display = "none";
+    });
+}
